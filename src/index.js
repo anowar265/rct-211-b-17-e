ddba8ada2e70fe730e6518f30d2f0f35adaa4702
Anowar Hossain <anowar3042@gmail.com>
2022-07-18 15:27:36 +0530

    init

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider as ReduxProvider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ReduxProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ReduxProvider>
   </BrowserRouter>
 );
 
